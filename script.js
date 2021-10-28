7d2126fa53da4b7322068b6ade8f864966c4270e
Gobind Puniani <gobindpuniani@gmail.com>
2021-10-27 17:25:08 -0700

    Modified column size and added circle shape for females

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const titanic = document.querySelector('#titanic')
 // Set some styles on the titanic
 // display flex, justifyContent center, alignItems flex-end
 titanic.style.display = 'grid'
-titanic.style.gridTemplateColumns = 'repeat(20, 10px)'
+// titanic.style.gridTemplateColumns = 'repeat(20, 10px)'
+titanic.style.gridTemplateColumns = 'repeat(34, 15px)'
 titanic.style.gridGap = '1px'
 
 // Map over the data and make a new element for each passenger
@@ -18,23 +19,31 @@ const passengers = data.map(p => {
 
 // Let's loop over each passenger and set some styles 
 passengers.forEach((p, i) => {
-  const { pclass, survived } = data[i].fields
-  p.style.width = '10px'
-  p.style.height = '10px'
+  const { pclass, survived, sex } = data[i].fields
+  // p.style.width = '10px'
+  // p.style.height = '10px'
+  p.style.width = '15px'
+  p.style.height = '15px'
   p.style.opacity = survived === 'Yes' ? '100%' : '50%' 
   p.style.backgroundColor = '#000'
+  p.style.borderRadius = sex === 'female' ? '50%' : 0
 })
 
 // Challenges - 
+
+// DONE 
 
 // Make the squares a little bigger 15px by 15px. 
 // You'll need to change both the gridTemplateColumn on the
 // titanic and the width and height of each passenger. 
 
 
+// DONE 
 
 // Change the number of columns on the titanic to 34
 
+
+// TODO 
 
 // Display each passenger as a circle if they are female. 
 // Do this by setting the borderRadius of each passenger. 
@@ -42,11 +51,13 @@ passengers.forEach((p, i) => {
 // in the data array by the index. 
 
 
+// DONE 
 
 // Display each passengers who did not survive as 
 // opacity 0.5. 
 
 
+// TODO
 
 // Set the backgroundColor of each passenger by their 
 // embarked value. There are three possible values: 
